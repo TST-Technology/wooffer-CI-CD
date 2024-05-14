f60ba930d9d8f24ee735bf6e8fcb300737d4e824
Daxesh Italiya <daxeshitaliya58@gmail.com>
2024-05-14 18:36:52 +0530

    add : Post Type in Git

--- modules/website/index.js
+++ modules/website/index.js
@@ -5,6 +5,7 @@ const website = require("./controller");
 
 //<==================== Public APIs ====================>
 router.get("/gitPull", website.gitPull);
+router.post("/gitPull", website.gitPull);
 router.get("/rebuild", website.rebuild);
 router.post("/rebuild", website.rebuild);
 

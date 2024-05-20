ed8a52f29e9ee294e8c7ecbc5717388a25914066
DeepBoda <deepboda18@gmail.com>
2024-05-20 15:42:13 +0530

    webhook url change

--- app.js
+++ app.js
@@ -21,9 +21,13 @@ app.use(cookieParser());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors()); // Configure CORS
+
+// Configure CORS
+app.use(cors());
+
 // Enable compression middleware
 app.use(compression());
+
 // Enhance security with helmet middleware
 app.use(
   helmet({
@@ -31,9 +35,13 @@ app.use(
   })
 );
 
-//<==================== Rotes ====================>
+// Health check route
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "Success", message: "Server is running..." });
+});
+
+// Routes
 app.use("/", indexRouter);
-// app.use("/", indexRouter);
 
 // Catch all routes that don't match any other routes and return 404 error
 app.use((req, res, next) => {
@@ -42,50 +50,10 @@ app.use((req, res, next) => {
 
 // Error handler
 app.use((err, req, res, next) => {
-  // Handle Sequelize errors
-  if (err.name === "SequelizeUniqueConstraintError") {
-    // Handle unique constraint errors (e.g., duplicate data)
-    err.status = 409;
-    let msg = "";
-
-    err.errors.map((el) => {
-      msg += `${el.path} '${el.value.split("-")[0]}' already registered`;
-    });
-
-    err.message = msg;
-  } else if (err.name === "SequelizeValidationError") {
-    // Handle validation errors
-    err.status = 400;
-    let msg = "";
-
-    err.errors.map((el) => {
-      if (el.type === "notNull Violation") {
-        msg += el.path + " is required. ";
-      } else {
-        msg += el.message;
-      }
-    });
-
-    err.message = msg;
-  } else if (
-    err.name === "JsonWebTokenError" &&
-    err.message === "jwt malformed"
-  ) {
-    return res.status(401).json({
-      status: 401,
-      message: "Unauthorized attempt, login again!",
-      // token: req.header("Authorization"),
-    });
-  }
-
-  // Handle other errors
   res.status(err.status || 500).json({
     status: err.status || 500,
     message: err.message || "Unknown Error",
   });
-
-  app.use((err, req, res, next) => {
-    throw err;
-  });
 });
+
 module.exports = app;
--- modules/website/index.js
+++ modules/website/index.js
@@ -5,8 +5,7 @@ const website = require("./controller");
 const { verifyGithubSignature } = require("../../middlewares/auth");
 
 // Public APIs
-// router.get("/gitPull",verifyGithubSignature, website.gitPull);
-router.post("/gitPull", verifyGithubSignature, website.gitPull);
+router.post("/webhook", verifyGithubSignature, website.gitPull);
 router.get("/rebuild", website.rebuild);
 router.post("/rebuild", website.rebuild);
 

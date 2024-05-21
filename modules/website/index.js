8143a0918daaeb98bbd799fb119ab6f60442cae1
DeepBoda <deepboda18@gmail.com>
2024-05-21 14:43:55 +0530

    middleware

--- modules/website/index.js
+++ modules/website/index.js
@@ -5,9 +5,9 @@ const website = require("./controller");
 const { verifyGithubSignature } = require("../../middlewares/auth");
 
 // Public APIs
-router.post("/webhook", website.gitPull);
-router.get("/webhook", website.gitPull);
+router.post("/webhook", verifyGithubSignature, website.gitPull);
+// router.get("/webhook", website.gitPull);
 router.post("/rebuild", website.rebuild);
-router.get("/rebuild", website.rebuild);
+// router.get("/rebuild", website.rebuild);
 
 module.exports = router;
